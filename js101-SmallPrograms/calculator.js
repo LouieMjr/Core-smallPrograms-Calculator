ea95d85f477d839ff8c6aba552b811b818163c9c
Louie Mendez <lmendezjr024@gmail.com>
2024-06-26 20:49:12 +0200

    added validation checks for numbers and operation entered in by user

--- js101-SmallPrograms/calculator.js
+++ js101-SmallPrograms/calculator.js
@@ -1,30 +1,61 @@
-const readline = require("readline-sync");
-const ask = readline.question
+import readline from "readline-sync";
+const ask = readline.question;
 
-console.log("Welcome to Calculator!");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?");
-const number1 = Number(ask());
+const invalidNumber = (number) => number.trimStart() === '' || Number.isNaN(Number(number));
 
-console.log("What's the second number?");
-const number2 = Number(ask());
+function checkForValidNumber(number) {
+  while (invalidNumber(number)) {
+    console.log('This isnt a number. Enter a valid number!');
+    number = ask();
+  }
+  return number;
+}
 
-console.log("What operation would you like to perform on these numbers? \n1) Add 2) Subtract 3) Multiply 4) Divide");
-const operation = ask()
+prompt("Welcome to Calculator!");
+
+prompt("What's the first number?");
+let number1 = ask();
+
+number1 = checkForValidNumber(number1);
+number1 = Number(number1);
+
+prompt("What's the second number?");
+let number2 = ask();
+
+number2 = checkForValidNumber(number2);
+number2 = Number(number2);
+
+prompt("What operation would you like to perform on these numbers? \n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = ask();
+
+function checkForValidOperation(string) {
+  const validStrings = ['1', '2', '3', '4'];
+  while (!validStrings.includes(string)) {
+    prompt('Must choose 1, 2, 3, or 4');
+    string = ask();
+  }
+  return string;
+}
+
+operation = checkForValidOperation(operation);
 
 let output;
 switch (operation) {
   case '1':
-    output = number1 + number2
+    output = number1 + number2;
     break;
   case '2':
-    output = number1 - number2
+    output = number1 - number2;
     break;
   case '3':
-    output = number1 * number2
+    output = number1 * number2;
     break;
   case '4':
-    output = number1 / number2
+    output = number1 / number2;
     break;
 }
 console.log(output);
