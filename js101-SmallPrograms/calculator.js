056d8ecfa61903a096b4cc2a534aed9877b37970
Louie Mendez <lmendezjr024@gmail.com>
2024-06-27 12:29:55 +0200

    can prompt user for more calculations if they want, instead of stopping after one calculation

--- js101-SmallPrograms/calculator.js
+++ js101-SmallPrograms/calculator.js
@@ -24,43 +24,52 @@ function checkForValidOperation(string) {
   return string;
 }
 
-
-prompt("Welcome to Calculator!");
-
-prompt("What's the first number?");
-let number1 = ask();
-
-number1 = checkForValidNumber(number1);
-number1 = Number(number1);
-
-prompt("What's the second number?");
-let number2 = ask();
-
-number2 = checkForValidNumber(number2);
-number2 = Number(number2);
-
-function pickOperation(operation) {
+function performOperationAndEvaluate(operation, num1, num2) {
   let output;
   switch (operation) {
     case '1':
-      output = number1 + number2;
+      output = num1 + num2;
       break;
     case '2':
-      output = number1 - number2;
+      output = num1 - num2;
       break;
     case '3':
-      output = number1 * number2;
+      output = num1 * num2;
       break;
     case '4':
-      output = number1 / number2;
+      output = num1 / num2;
       break;
   }
   return output;
 }
 
-prompt("What operation would you like to perform on these numbers? \n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = ask();
-operation = checkForValidOperation(operation);
+function promptForNumbersAndOperation() {
+  prompt("What's the first number?");
+  let number1 = ask();
 
-console.log(pickOperation(operation));
+  number1 = checkForValidNumber(number1);
+  number1 = Number(number1);
 
+  prompt("What's the second number?");
+  let number2 = ask();
+
+  number2 = checkForValidNumber(number2);
+  number2 = Number(number2);
+
+  prompt("What operation would you like to perform on these numbers? \n1) Add 2) Subtract 3) Multiply 4) Divide");
+  let operation = ask();
+  operation = checkForValidOperation(operation);
+
+  console.log(performOperationAndEvaluate(operation, number1, number2));
+  return restartCal();
+}
+
+function restartCal() {
+  prompt('Do you want to perform another calculation? Enter Y or N');
+  const result = ask();
+  if (result[0].toUpperCase() === 'Y') return promptForNumbersAndOperation();
+  return 'No more calculations for now. Rest!';
+}
+
+prompt("Welcome to Calculator!");
+console.log(promptForNumbersAndOperation());
