76f579f18e67741512c9979eef395d35d5be0f4e
Louie Mendez <lmendezjr024@gmail.com>
2024-06-27 21:29:52 +0200

    added ability to select different languages

--- js101/smallPrograms-Lesson2/calculator/calculator.js
+++ js101/smallPrograms-Lesson2/calculator/calculator.js
@@ -1,4 +1,4 @@
-import messageObj from '../calculatorMessages.json' assert {type: 'json'};
+import messageObj from './calculatorMessages.json' assert { type: "json" };
 import readline from "readline-sync";
 const ask = readline.question;
 
@@ -10,7 +10,7 @@ const invalidNumber = (number) => number.trimStart() === '' || Number.isNaN(Numb
 
 function checkForValidNumber(number) {
   while (invalidNumber(number)) {
-    prompt(messageObj.invalidNum);
+    prompt(messageObj[language].invalidNum);
     number = ask();
   }
   return number;
@@ -19,7 +19,7 @@ function checkForValidNumber(number) {
 function checkForValidOperation(string) {
   const validStrings = ['1', '2', '3', '4'];
   while (!validStrings.includes(string)) {
-    prompt(messageObj.chooseValidString);
+    prompt(messageObj[language].chooseValidNum);
     string = ask();
   }
   return string;
@@ -45,32 +45,47 @@ function performOperationAndEvaluate(operation, num1, num2) {
 }
 
 function promptForNumbersAndOperation() {
-  prompt(messageObj.firstNum);
+  prompt(messageObj[language].firstNum);
   let number1 = ask();
 
   number1 = checkForValidNumber(number1);
   number1 = Number(number1);
 
-  prompt(messageObj.secondNum);
+  prompt(messageObj[language].secondNum);
   let number2 = ask();
 
   number2 = checkForValidNumber(number2);
   number2 = Number(number2);
 
-  prompt(messageObj.operation);
+  prompt(messageObj[language].operation);
   let operation = ask();
   operation = checkForValidOperation(operation);
 
   console.log(performOperationAndEvaluate(operation, number1, number2));
-  return restartCal();
+  return restartCalc();
 }
 
-function restartCal() {
-  prompt(messageObj.restart);
+function restartCalc() {
+  prompt(messageObj[language].restart);
   const result = ask();
   if (result[0].toUpperCase() === 'Y') return promptForNumbersAndOperation();
-  return messageObj.finish;
+  return messageObj[language].finish;
 }
+function validLanguage() {
+  prompt(messageObj.language)
+  let language = ask();
+  language = language.toLowerCase();
+  
+  const validStrings = ['en', 'es', 'fr', 'de', 'ja', 'ar']
+  
+  while (!validStrings.includes(language)) {
+    prompt(messageObj.en.chooseValidLanguage);
+    language = ask();
+    language = language.toLowerCase();
+  }
+  return language;
+};
 
-prompt(messageObj.greet);
+prompt(messageObj.en.greet);
+const language = validLanguage();
 console.log(promptForNumbersAndOperation());
